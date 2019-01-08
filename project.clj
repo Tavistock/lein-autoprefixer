22f235d64750d16f06c7a56133b2f5b975f0a2f7
Travis McNeill <tmcneill@horizoninvestments.com>
2019-01-08 14:50:23 -0500

    changed url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-autoprefixer "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "For running autoprefixer in lein."
+  :url "https://github.com/Tavistock/lein-autoprefixer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
